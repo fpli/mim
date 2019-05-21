c3da3112db6dba8989551fddae7da454c3cf31e9
Li <fangpeng.li01@sap.com>
2019-05-21 13:35:53 +0800

    Initial commit

---
+++ app/src/main/java/com/sap/mim/adapter/holder/ChatAcceptViewHolder.java
@@ -0,0 +1,88 @@
+package com.sap.mim.adapter.holder;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.jude.easyrecyclerview.adapter.BaseViewHolder;
+import com.sap.mim.R;
+import com.sap.mim.adapter.ChatAdapter;
+import com.sap.mim.entity.MessageInfo;
+import com.sap.mim.util.Utils;
+import com.sap.mim.widget.BubbleImageView;
+import com.sap.mim.widget.GifTextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * 作者：Rance on 2016/11/29 10:47
+ * 邮箱：rance935@163.com
+ */
+public class ChatAcceptViewHolder extends BaseViewHolder<MessageInfo> {
+
+    @Bind(R.id.chat_item_date)
+    TextView chatItemDate;
+    @Bind(R.id.chat_item_header)
+    ImageView chatItemHeader;
+    @Bind(R.id.chat_item_content_text)
+    GifTextView chatItemContentText;
+    @Bind(R.id.chat_item_content_image)
+    BubbleImageView chatItemContentImage;
+    @Bind(R.id.chat_item_voice)
+    ImageView chatItemVoice;
+    @Bind(R.id.chat_item_layout_content)
+    LinearLayout chatItemLayoutContent;
+    @Bind(R.id.chat_item_voice_time)
+    TextView chatItemVoiceTime;
+    private ChatAdapter.onItemClickListener onItemClickListener;
+    private Handler handler;
+
+    public ChatAcceptViewHolder(ViewGroup parent, ChatAdapter.onItemClickListener onItemClickListener, Handler handler) {
+        super(parent, R.layout.item_chat_accept);
+        ButterKnife.bind(this, itemView);
+        this.onItemClickListener = onItemClickListener;
+        this.handler = handler;
+    }
+
+    @Override
+    public void setData(MessageInfo data) {
+        chatItemDate.setText(data.getTime() != null ? data.getTime() : "");
+        Glide.with(getContext()).load(data.getHeader()).into(chatItemHeader);
+        chatItemHeader.setOnClickListener((v) -> {
+                onItemClickListener.onHeaderClick(getDataPosition());
+        });
+        if (data.getContent() != null) {
+            chatItemContentText.setSpanText(handler, data.getContent(), true);
+            chatItemVoice.setVisibility(View.GONE);
+            chatItemContentText.setVisibility(View.VISIBLE);
+            chatItemLayoutContent.setVisibility(View.VISIBLE);
+            chatItemVoiceTime.setVisibility(View.GONE);
+            chatItemContentImage.setVisibility(View.GONE);
+        } else if (data.getImageUrl() != null) {
+            chatItemVoice.setVisibility(View.GONE);
+            chatItemLayoutContent.setVisibility(View.GONE);
+            chatItemVoiceTime.setVisibility(View.GONE);
+            chatItemContentText.setVisibility(View.GONE);
+            chatItemContentImage.setVisibility(View.VISIBLE);
+            Glide.with(getContext()).load(data.getImageUrl()).into(chatItemContentImage);
+            chatItemContentImage.setOnClickListener((v) -> {
+                    onItemClickListener.onImageClick(chatItemContentImage, getDataPosition());
+            });
+        } else if (data.getFilepath() != null) {
+            chatItemVoice.setVisibility(View.VISIBLE);
+            chatItemLayoutContent.setVisibility(View.VISIBLE);
+            chatItemContentText.setVisibility(View.GONE);
+            chatItemVoiceTime.setVisibility(View.VISIBLE);
+            chatItemContentImage.setVisibility(View.GONE);
+            chatItemVoiceTime.setText(Utils.formatTime(data.getVoiceTime()));
+            chatItemLayoutContent.setOnClickListener((v) -> {
+                    onItemClickListener.onVoiceClick(chatItemVoice, getDataPosition());
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/sap/mim/ui/activity/MainActivity.java
@@ -0,0 +1,276 @@
+package com.sap.mim.ui.activity;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.jude.easyrecyclerview.EasyRecyclerView;
+import com.sap.mim.R;
+import com.sap.mim.adapter.ChatAdapter;
+import com.sap.mim.adapter.CommonFragmentPagerAdapter;
+import com.sap.mim.entity.FullImageInfo;
+import com.sap.mim.entity.MessageInfo;
+import com.sap.mim.ui.fragment.ChatEmotionFragment;
+import com.sap.mim.ui.fragment.ChatFunctionFragment;
+import com.sap.mim.util.Constants;
+import com.sap.mim.util.GlobalOnItemClickManagerUtils;
+import com.sap.mim.util.MediaManager;
+import com.sap.mim.widget.EmotionInputDetector;
+import com.sap.mim.widget.NoScrollViewPager;
+import com.sap.mim.widget.StateButton;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * 作者：Rance on 2016/11/29 10:47
+ * 邮箱：rance935@163.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.chat_list)
+    EasyRecyclerView chatList;
+    @Bind(R.id.emotion_voice)
+    ImageView emotionVoice;
+    @Bind(R.id.edit_text)
+    EditText editText;
+    @Bind(R.id.voice_text)
+    TextView voiceText;
+    @Bind(R.id.emotion_button)
+    ImageView emotionButton;
+    @Bind(R.id.emotion_add)
+    ImageView emotionAdd;
+    @Bind(R.id.emotion_send)
+    StateButton emotionSend;
+    @Bind(R.id.viewpager)
+    NoScrollViewPager viewpager;
+    @Bind(R.id.emotion_layout)
+    RelativeLayout emotionLayout;
+
+    private EmotionInputDetector mDetector;
+    private ArrayList<Fragment> fragments;
+    private ChatEmotionFragment chatEmotionFragment;
+    private ChatFunctionFragment chatFunctionFragment;
+    private CommonFragmentPagerAdapter adapter;
+
+    private ChatAdapter chatAdapter;
+    private LinearLayoutManager layoutManager;
+    private List<MessageInfo> messageInfos;
+    //录音相关
+    int animationRes = 0;
+    int res = 0;
+    AnimationDrawable animationDrawable = null;
+    private ImageView animView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        EventBus.getDefault().register(this);
+        initWidget();
+        loadData();
+    }
+
+    private void initWidget() {
+        fragments = new ArrayList<>();
+        chatEmotionFragment = new ChatEmotionFragment();
+        fragments.add(chatEmotionFragment);
+        chatFunctionFragment = new ChatFunctionFragment();
+        fragments.add(chatFunctionFragment);
+        adapter = new CommonFragmentPagerAdapter(getSupportFragmentManager(), fragments);
+        viewpager.setAdapter(adapter);
+        viewpager.setCurrentItem(0);
+
+        mDetector = EmotionInputDetector.with(this)
+                .setEmotionView(emotionLayout)
+                .setViewPager(viewpager)
+                .bindToContent(chatList)
+                .bindToEditText(editText)
+                .bindToEmotionButton(emotionButton)
+                .bindToAddButton(emotionAdd)
+                .bindToSendButton(emotionSend)
+                .bindToVoiceButton(emotionVoice)
+                .bindToVoiceText(voiceText)
+                .build();
+
+        GlobalOnItemClickManagerUtils globalOnItemClickListener = GlobalOnItemClickManagerUtils.getInstance(this);
+        globalOnItemClickListener.attachToEditText(editText);
+
+        chatAdapter = new ChatAdapter(this);
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        chatList.setLayoutManager(layoutManager);
+        chatList.setAdapter(chatAdapter);
+        chatList.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                switch (newState) {
+                    case RecyclerView.SCROLL_STATE_IDLE:
+                        chatAdapter.handler.removeCallbacksAndMessages(null);
+                        chatAdapter.notifyDataSetChanged();
+                        break;
+                    case RecyclerView.SCROLL_STATE_DRAGGING:
+                        chatAdapter.handler.removeCallbacksAndMessages(null);
+                        mDetector.hideEmotionLayout(false);
+                        mDetector.hideSoftInput();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+        chatAdapter.addItemClickListener(itemClickListener);
+    }
+
+    /**
+     * item点击事件
+     */
+    private ChatAdapter.onItemClickListener itemClickListener = new ChatAdapter.onItemClickListener() {
+
+        @Override
+        public void onHeaderClick(int position) {
+            Toast.makeText(MainActivity.this, "onHeaderClick", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onImageClick(View view, int position) {
+            int location[] = new int[2];
+            view.getLocationOnScreen(location);
+            FullImageInfo fullImageInfo = new FullImageInfo();
+            fullImageInfo.setLocationX(location[0]);
+            fullImageInfo.setLocationY(location[1]);
+            fullImageInfo.setWidth(view.getWidth());
+            fullImageInfo.setHeight(view.getHeight());
+            fullImageInfo.setImageUrl(messageInfos.get(position).getImageUrl());
+            EventBus.getDefault().postSticky(fullImageInfo);
+            startActivity(new Intent(MainActivity.this, FullImageActivity.class));
+            overridePendingTransition(0, 0);
+        }
+
+        @Override
+        public void onVoiceClick(final ImageView imageView, final int position) {
+            if (animView != null) {
+                animView.setImageResource(res);
+                animView = null;
+            }
+            switch (messageInfos.get(position).getType()) {
+                case 1:
+                    animationRes = R.drawable.voice_left;
+                    res = R.mipmap.icon_voice_left3;
+                    break;
+                case 2:
+                    animationRes = R.drawable.voice_right;
+                    res = R.mipmap.icon_voice_right3;
+                    break;
+            }
+            animView = imageView;
+            animView.setImageResource(animationRes);
+            animationDrawable = (AnimationDrawable) imageView.getDrawable();
+            animationDrawable.start();
+            MediaManager.playSound(messageInfos.get(position).getFilepath(), (mp) -> animView.setImageResource(res) );
+        }
+    };
+
+    /**
+     * 构造初始聊天数据
+     */
+    private void loadData() {
+        messageInfos = new ArrayList<>();
+
+        MessageInfo messageInfo = new MessageInfo();
+        messageInfo.setContent("你好，欢迎使用Rance的聊天界面框架");
+        messageInfo.setType(Constants.CHAT_ITEM_TYPE_LEFT);
+        messageInfo.setHeader("http://tupian.enterdesk.com/2014/mxy/11/2/1/12.jpg");
+        messageInfos.add(messageInfo);
+
+        MessageInfo messageInfo1 = new MessageInfo();
+        messageInfo1.setFilepath("http://www.trueme.net/bb_midi/welcome.wav");
+        messageInfo1.setVoiceTime(3000);
+        messageInfo1.setType(Constants.CHAT_ITEM_TYPE_RIGHT);
+        messageInfo1.setSendState(Constants.CHAT_ITEM_SEND_SUCCESS);
+        messageInfo1.setHeader("http://img.dongqiudi.com/uploads/avatar/2014/10/20/8MCTb0WBFG_thumb_1413805282863.jpg");
+        messageInfos.add(messageInfo1);
+
+        MessageInfo messageInfo2 = new MessageInfo();
+        messageInfo2.setImageUrl("http://img4.imgtn.bdimg.com/it/u=1800788429,176707229&fm=21&gp=0.jpg");
+        messageInfo2.setType(Constants.CHAT_ITEM_TYPE_LEFT);
+        messageInfo2.setHeader("http://tupian.enterdesk.com/2014/mxy/11/2/1/12.jpg");
+        messageInfos.add(messageInfo2);
+
+        MessageInfo messageInfo3 = new MessageInfo();
+        messageInfo3.setContent("[微笑][色][色][色]");
+        messageInfo3.setType(Constants.CHAT_ITEM_TYPE_RIGHT);
+        messageInfo3.setSendState(Constants.CHAT_ITEM_SEND_ERROR);
+        messageInfo3.setHeader("http://img.dongqiudi.com/uploads/avatar/2014/10/20/8MCTb0WBFG_thumb_1413805282863.jpg");
+        messageInfos.add(messageInfo3);
+
+        chatAdapter.addAll(messageInfos);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void messageEventBus(final MessageInfo messageInfo) {
+        messageInfo.setHeader("http://img.dongqiudi.com/uploads/avatar/2014/10/20/8MCTb0WBFG_thumb_1413805282863.jpg");
+        messageInfo.setType(Constants.CHAT_ITEM_TYPE_RIGHT);
+        messageInfo.setSendState(Constants.CHAT_ITEM_SENDING);
+        messageInfos.add(messageInfo);
+        chatAdapter.add(messageInfo);
+        chatList.scrollToPosition(chatAdapter.getCount() - 1);
+
+        new Handler().postDelayed(() -> {
+                messageInfo.setSendState(Constants.CHAT_ITEM_SEND_SUCCESS);
+                chatAdapter.notifyDataSetChanged();
+        }, 2000);
+
+        new Handler().postDelayed(() -> {
+                MessageInfo message = new MessageInfo();
+                message.setContent("系统模拟消息回复");
+                message.setType(Constants.CHAT_ITEM_TYPE_LEFT);
+                message.setHeader("http://tupian.enterdesk.com/2014/mxy/11/2/1/12.jpg");
+                messageInfos.add(message);
+                chatAdapter.add(message);
+                chatList.scrollToPosition(chatAdapter.getCount() - 1);
+        }, 3000);
+    }
+
+    /**
+     * 用户点击后退键
+     */
+    @Override
+    public void onBackPressed() {
+        if (!mDetector.interceptBackPress()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+        EventBus.getDefault().removeStickyEvent(this);
+        EventBus.getDefault().unregister(this);
+    }
+}
