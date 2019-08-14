23e925a602557a473f63a6d1ae5bc73cd0c433b2
fpli <1394220859@qq.com>
2019-08-14 12:16:53 +0800

    命名标准化

---
+++ app/src/main/java/com/sap/mim/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sap.mim.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sap.mim.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
